d568212e360d170a5bce4e4aa86c245a505ba616
magicbing <932201328@qq.com>
2018-10-17 00:54:33 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,77 @@
 <template>
   <div id="app">
+    <mu-appbar style="width: 100%;" color="primary">
+      <mu-button icon slot="left">
+        <mu-icon value="menu"></mu-icon>
+      </mu-button>
+      <mu-container class="" slot="default">
+        <mu-button small color="secondary" :to="item"
+         v-for="item in this.$store.state.today.category" :key="item">{{item}}</mu-button>
+      </mu-container>
+      <!-- <mu-menu slot="right">
+        <mu-button flat>MENU</mu-button>
+        <mu-list slot="content">
+          <mu-list-item button>
+            <mu-list-item-content>
+              <mu-list-item-title>Menu Item 1</mu-list-item-title>
+            </mu-list-item-content>
+          </mu-list-item>
+          <mu-list-item button>
+            <mu-list-item-content>
+              <mu-list-item-title>Menu Item 2</mu-list-item-title>
+            </mu-list-item-content>
+          </mu-list-item>
+        </mu-list>
+      </mu-menu> -->
+    </mu-appbar>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
     <mu-container class="button-wrapper">
-      <mu-button color="primary">Primary</mu-button>
+      <mu-button color="primary" v-on:click="test">test</mu-button>
     </mu-container>
   </div>
 
 </template>
 
-<style>
+<script>
+export default {
+  name: "App",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      a: 1
+    };
+  },
+  created: function() {
+    // `this` 指向 vm 实例
+    // console.log('a is: ' + this.a)
+    // console.log( 2 )
+    this.$store.dispatch("getToday");
+    console.log(this.$store.state.today.category);
+  },
+  methods: {
+    test: function() {
+      console.log(this.$store.state.today);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  overflow: scroll;
+}
+.container a {
+  vertical-align: middle;
+}
+/* 下面是cli的css */
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/store.js
+++ src/store.js
@@ -1,16 +1,53 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from 'axios'
+axios.defaults.baseURL = 'https://gank.io/api';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    qqq: 111,
+    today: {}
   },
   mutations: {
-
+    setToday(state, data) {
+      state.today = data
+    }
   },
   actions: {
+    getToday({ commit }) {
+      axios({
+        method: 'get',
+        url: '/today',
+        // data: this.state.qqq
+      }).then(function (response) {
+        // console.log(response.data);
+        // this.state.today = response.data
+        // this.setToday
+        commit("setToday", response.data)
+        console.log(response.data)
+      });
+    },
+    setToday({ commit }) {
+      commit('setToday')
+    }
+    /////// 例子: 
+    /* saveFrom(context) {
+      axios({
+        method: 'post',
+        url: '/user',
+        data: context.state.test02
+      })
+    } */
+    /* 在组件中发送请求的时候，需要使用 this.$store.dispatch 来分发
+    methods: {
+      submitForm () {
+        this.$store.dispatch('saveForm')
+      }
+    }
+    submitForm 是绑定在组件上的一个方法，将触发 saveForm，从而通过 axios 向服务器发送请求 
+    */
 
   }
 })

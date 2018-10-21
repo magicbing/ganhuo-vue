81f2fb5dbb92ca17f87e5417e35a53cc62c28c8b
magicbing <932201328@qq.com>
2018-10-21 20:59:19 +0800

    config

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+  // 部署生产环境和开发环境下的URL。
+  // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
+  //例如 https://www.my-app.com/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.my-app.com/my-app/，则设置 baseUrl 为 /my-app/。
+  baseUrl: "./",
+  //指定生成的 index.html 的输出路径  (打包之后，改变系统默认的index.html的文件名)
+  // indexPath: "myIndex.html",
+  //默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。你可以通过将这个选项设为 false 来关闭文件名哈希。(false的时候就是让原来的文件名不改变)
+  filenameHashing: false,
+  //   lintOnSave：{ type:Boolean default:true } 问你是否使用eslint
+  // lintOnSave: true,
+  //如果你想要在生产构建时禁用 eslint-loader，你可以用如下配置
+  // lintOnSave: process.env.NODE_ENV !== 'production',
+ 
+  //是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。(默认false)
+  // runtimeCompiler: false,
+ 
+  productionSourceMap: false,
+ 
+};
